470b1d38a903aafccbfd77c3f4c9328bd83e53d2
dgsousa <sousa.danielg@gmail.com>
2017-03-31 12:25:28 -0700

    refactored

--- js/computer.js
+++ js/computer.js
@@ -1,236 +1,118 @@
 function Computer(symbol) {
-  'use strict';
-  var filled = [];
-  var rowCombos = [[34, 2],[6, 5],[36, 1], [65, 7], [192, 0], [129, 6], [24, 8], [264, 4], [272, 3], [10, 6], 
-                   [66, 3], [72, 1], [288, 0], [33, 8], [257, 5], [20, 7], [132, 4], [144, 2], [18, 0], [3, 4],
-                   [17, 1], [12, 0], [5, 3], [9, 2]]; 
+	'use strict';
+	var filled = [];
+	var rowCombos = [[34, 2],[6, 5],[36, 1], [65, 7], [192, 0], [129, 6], [24, 8], [264, 4], [272, 3], [10, 6], 
+					[66, 3], [72, 1], [288, 0], [33, 8], [257, 5], [20, 7], [132, 4], [144, 2], [18, 0], [3, 4],
+					[17, 1], [12, 0], [5, 3], [9, 2]]; 
   
-  this.symbol = symbol;
+	this.symbol = symbol;
   
-  this.move = function(num, array, score) {
-    for(var i = 0; i < 9; i++) {
-      var digit = i;
-      if(array[i].marked && filled.indexOf(digit) === -1) {
-        filled.push(digit);
-      }
-    }
-    
-  	var index;
-    switch(num) {
-    	case 1:
-    	index = this.firstMove();
-      filled.push(index);
-      return index;
-    	break; 
+	this.move = function(movenumber, squares, score) {
+		var index;
+		for(var i = 0; i < 9; i++) {
+			if(squares[i].marked && filled.indexOf(i) === -1) {
+				filled.push(i);
+			}
+		}
 
-    	case 2:
-    	index = this.secondMove();
-      filled.push(index);
-      return index;
-    	break;
-
-      case 3:
-      index = this.thirdMove();
-      filled.push(index)
-      return index;
-      break;
-
-      case 4:
-      index = this.fourthMove(score);
-      filled.push(index);
-      return index;
-      break;
-    }
-
-    if(num >= 5) {
-      var special;
-      index = this.fifthMove(score);
-      filled.push(index);
-      return index;
-    }
-
-  };
-  this.firstMove = function() {
-  	var random = Math.floor(Math.random()*5);
-  	return random;
-  };
+		index = (
+			movenumber === 1 ? this.getRandom(0, 5) :
+			movenumber === 2 ? this.secondMove() :
+			movenumber === 3 ? this.thirdMove() :
+			movenumber === 4 ? this.fourthMove(score) :
+			this.fifthMove(score)
+		)
+		filled.push(index);
+		return index;
+	};
   
-  this.secondMove = function() {
-  	if(filled[0] !== 0) {
-  		return 0;
-  	}
-  	else {
-  		var random = Math.floor((Math.random() * 4) + 1);
-  		return random;
-  	}
-  };
+	this.secondMove = function() {
+		return filled[0] === 0 ? this.getRandom(1, 5) : 0
+	};
   
-  this.thirdMove = function() { 
-  		if(filled[0] === 0) {
-        if(filled[1] < 5) {
-          return this.verticalRow(filled[1])
-        }
-        else {
-          return this.secondMove();
-        }
-      }
-      else {
-        if(this.isDiagonal(filled[0], filled[1])) {
-          return this.verticalRow(filled[1])
-        }
-        else if(filled[1] === 0) {
-          return this.diagonal(filled[0]);
-        }
-        else {
-          return 0;
-        }
-      }	
-  }
+	this.thirdMove = function() { 
+		if(filled[0] === 0) {
+			return filled[1] < 5 ? this.verticalRow(filled[1]) : this.secondMove();
+		} else {
+			return this.isDiagonal(filled[0], filled[1])	? 	this.verticalRow(filled[1]) : 
+										filled[1] === 0	 	?	this.diagonal(filled[0]) 	: 	0
+		}	
+	}
 
-  this.fourthMove = function(playerScore) {
-    if(this.rowDetector(playerScore)) {
-      return this.rowDetector(playerScore);
-    }
-    else if(this.isDiagonal(filled[0], filled[2])) {
-      var random = Math.floor((Math.random() * 4) + 5);
-      return random;
-    }
-    else if (filled[2] > 4) {
-      return this.adjacent(filled[2]);
-    };
-  }
+	this.fourthMove = function(playerScore) {
+		return 	this.rowDetector(playerScore)	|| 
+			(
+			this.isDiagonal(filled[0], filled[2])	? 	this.getRandom(5, 9)		: 
+									filled[2] > 4	?	this.adjacent(filled[2])	: 	this.getRandom(1, 5)
+		);
+	}
 
-  this.fifthMove = function(playerScore) {
-    if(this.rowDetector(this.score)) {
-      return this.rowDetector(this.score);
-    }
+	this.fifthMove = function(playerScore) {
+		return	this.rowDetector(this.score) || this.rowDetector(playerScore) ||
+				(
+				this.horzAdj(filled[0], filled[1])	?	this.verticalRow(filled[0])		:
+				this.vertAdj(filled[0], filled[1])	?	this.horizontalRow(filled[0])	: 	this.getRandom(0, 9)
+			);
+	}
 
-    else if(this.rowDetector(playerScore)) {
-      return this.rowDetector(playerScore);
-    }
+	this.rowDetector = function(num1) {
+		for(var i = 0; i < rowCombos.length; i++) {
+			if(((num1 & rowCombos[i][0]) === rowCombos[i][0]) && filled.indexOf(rowCombos[i][1]) === -1) {       
+				return rowCombos[i][1];
+			}
+		} return false;
+	}
 
-    else if(this.horzAdj(filled[0], filled[1])) {
-      return this.verticalRow(filled[0]);
-  }
-    else if(this.vertAdj(filled[0], filled[1])) {
-      return this.horizontalRow(filled[0]);
-    }
-    else {
-      return this.getRandom(0,9);
-    }
-  }
-
-  this.rowDetector = function(num1) {
-    for(var i = 0; i < rowCombos.length; i++) {
-        if(((num1 & rowCombos[i][0]) === rowCombos[i][0]) && filled.indexOf(rowCombos[i][1]) === -1) {       
-          return rowCombos[i][1];
-        }
-    } return false;
-  }
-
-  this.isDiagonal = function(num1,num2) {
-    if((num1 === 1 && num2 === 4) || (num1 === 4 && num2 === 1) || (num1 === 2 && num2 === 3) || (num1 === 3 && num2 === 2)) {
-      return true;
-    } 
-    return false;
-  }
+	this.isDiagonal = function(num1,num2) {
+		return ((num1 === 1 && num2 === 4) || 
+				(num1 === 4 && num2 === 1) || 
+				(num1 === 2 && num2 === 3) || 
+				(num1 === 3 && num2 === 2))
+	}
   
-  this.diagonal = function(num) {
-    switch(num) {
-      case 1:
-      return 4;
-      break;
-
-      case 2:
-      return 3;
-      break;
-
-      case 3:
-      return 2;
-      break;
-
-      case 4:
-      return 1;
-      break;
-    }
-  };
+	this.diagonal = function(num) {
+		return (num === 1 ? 4 :
+				num === 4 ? 1 :
+				num === 2 ? 3 :
+				num === 3 ? 2 : false)
+	};
   
-  this.verticalRow = function(num) {
-    switch(num) {
-      case 1:
-      return 3;
-      break;
+	this.verticalRow = function(num) {
+		return (num === 1 ? 3 :
+				num === 3 ? 1 :
+				num === 2 ? 4 :
+				num === 4 ? 2 : false)
+	};
 
-      case 2:
-      return 4;
-      break;
-
-      case 3:
-      return 1;
-      break;
-
-      case 4:
-      return 2;
-      break;
-    }
-  };
-
-  this.horizontalRow = function(num) {
-    switch(num) {
-      case 1:
-      return 2;
-      break;
-
-      case 2:
-      return 1;
-      break;
-
-      case 3:
-      return 4;
-      break;
-
-      case 4:
-      return 3;
-      break;
-    }
-  };
+	this.horizontalRow = function(num) {
+		return (num === 1 ? 2 :
+				num === 2 ? 1 :
+				num === 3 ? 4 :
+				num === 4 ? 3 : false)
+	};
   
-  this.adjacent = function(num) {
-    if(num === 5 || num === 6) {
-      return 1;
-    }
-    if(num === 7 || num === 8) {
-      return 4;
-    }
-  }
+	this.adjacent = function(num) {
+		return (num === 5 || num === 6 ? 1 :
+				num === 7 || num === 8 ? 4 : false)
+	}
   
-  this.horzAdj = function(num1, num2) {
-    if((num1 === 1 || num1 === 2) && (num2 === 5)) {
-      return true;
-    }
-    if((num1 === 3 || num1 === 4) && (num2 === 8)) {
-      return true;
-    } return false;
-  };
+	this.horzAdj = function(num1, num2) {
+		return ((num1 === 1 || num1 === 2) && (num2 === 5) ? true :
+				(num1 === 3 || num1 === 4) && (num2 === 8) ? true : false)
+	};
 
-  this.vertAdj = function(num1, num2) {
-    if((num1 === 1 || num1 === 3) && (num2 === 6)) {
-      return true;
-    }
-    if((num1 === 2 || num1 === 4) && (num2 === 7)) {
-      return true;
-    } return false;
-  }
+	this.vertAdj = function(num1, num2) {
+		return ((num1 === 1 || num1 === 3) && (num2 === 6) ? true :
+				(num1 === 2 || num1 === 4) && (num2 === 7) ? true : false)
+	}
 
-  this.getRandom = function(min, max) {
-    var random = Math.floor((Math.random() * (max - min) + min));
-    if(filled.indexOf(random) !== -1) {
-      return this.getRandom(min, max);
-    }
-    else {
-      return random;
-    }
-  }  
+	this.getRandom = function(min, max) {
+		var random;
+		do {
+			random = Math.floor((Math.random() * (max - min) + min));
+		} while (filled.indexOf(random) != -1)
+			return random;
+	}
 }
 
 Computer.prototype = new Player();
--- js/game.js
+++ js/game.js
@@ -1,113 +1,80 @@
 function Game() {
-  var thePlayer,
-      theComputer,
-      theBoard = new Board(),
-      gameEnded = false,
-      moveNumber = 1;
-      self = this;
+	var thePlayer,
+		theComputer,
+		theBoard = new Board(),
+		gameEnded = false,
+		moveNumber = 1;
+		self = this;
   
-      
-  this.selectSymbol = function(char) {
-    if(char === 'X') {
-      theComputer = new Computer('O');
-      thePlayer = new Player('X');
-      this.currentPlayer = thePlayer;
-    }
-    else if(char === 'O') {
-      theComputer = new Computer('X');
-      thePlayer = new Player('O');
-      this.currentPlayer = theComputer; 
-      this.computerTurn();  
-    }
-  }
-      
-  this.playerSymbol = function() {
-    return this.currentPlayer.getSymbol();
-  }
-      
-  this.playerScore = function() {
-    return thePlayer.getScore();
-  }
+	  
+	this.selectSymbol = function(char) {
+		if(char === 'X') {
+			theComputer = new Computer('O');
+			thePlayer = new Player('X');
+			this.currentPlayer = thePlayer;
+		} else if(char === 'O') {
+			theComputer = new Computer('X');
+			thePlayer = new Player('O');
+			this.currentPlayer = theComputer; 
+			this.executeMove(this.computerTurn(), thePlayer);  
+		}
+	}
+	  
+	this.currentSymbol = function() {
+		return this.currentPlayer.getSymbol();
+	}
+	  
+	this.playerScore = function() {
+		return thePlayer.getScore();
+	}
   
-  this.computerTurn = function() {
-    if(!gameEnded) {
-      if(this.currentPlayer === theComputer) {
-        var index = this.currentPlayer.move(moveNumber, theBoard.returnSquares(), this.playerScore());
-        var symbol = this.playerSymbol();
-        if(theBoard.isSquareEmpty(index)) {
-          theBoard.setSquareOccupied(index);
-          this.currentPlayer.updateScore(theBoard.getValue(index));
-          moveNumber += 1;
-          interface.render(symbol, index);
+	this.computerTurn = function() {
+		return this.currentPlayer.move(moveNumber, theBoard.returnSquares(), this.playerScore());	
+	}
+	  
+	this.takeaTurn = function(index) {
+		this.executeMove(index, theComputer);
+		setTimeout(function() {
+			if(!gameEnded && self.currentPlayer === theComputer) {
+				self.executeMove(self.computerTurn(), thePlayer);
+			}
+		}, 1000);
+	}
 
-          if(this.currentPlayer.isWinner()) {
-            setTimeout(function() {
-              interface.renderGame('You Lose!');
-            }, 1000);
-            gameEnded = true;
-          }
-          
-          if(this.isTie()) {
-            setTimeout(function() {
-              interface.renderGame('Tie Game!');
-            }, 1000);
-            gameEnded = true;
-          }
-          
-          this.currentPlayer = thePlayer;
-        } 
-      }
-    } 
-  }
-      
-  this.takeaTurn = function(squareIndex) {
+	this.executeMove = function(index, player) {
+		if(!gameEnded && this.currentPlayer != player && theBoard.isSquareEmpty(index)) {
+			theBoard.setSquareOccupied(index);
+			this.currentPlayer.updateScore(theBoard.getValue(index));
+			moveNumber += 1;
+			interface.render(this.currentSymbol(), index);         
+			this.ifOver();
+			this.currentPlayer = player;
+		}
+	}
 
-    if(!gameEnded) {
-      if(this.currentPlayer = thePlayer) {
-        if(theBoard.isSquareEmpty(squareIndex)) {
-          theBoard.setSquareOccupied(squareIndex);
-          this.currentPlayer.updateScore(theBoard.getValue(squareIndex));
-          moveNumber += 1;
-          interface.render(this.playerSymbol(), squareIndex);         
-          if(this.currentPlayer.isWinner()) {
-            setTimeout(function() {
-              interface.renderGame('You Win!');
-            }, 1000);
-            gameEnded = true;
-          }
-    
-          if(this.isTie()) {
-            setTimeout(function() {
-              interface.renderGame('Tie Game!');
-            }, 1000);
-            gameEnded = true;
-          }
-    
-          this.currentPlayer = theComputer;
-          setTimeout(function() {
-            self.computerTurn();
-          },1000);
-        }
-      }
-    }  
-  }
-       
-  this.isTie = function() {
-    if(theBoard.isAllFilledIn()) {
-      if(!this.currentPlayer.isWinner()) {
-        return true;
-      }
-    } return false;
-  }
-      
-  this.resetGame = function() {
-    theBoard.resetSquares();
-    thePlayer.resetScore();
-    theComputer.resetScore();
-    gameEnded = false;
-    moveNumber = 1;
-    interface.renderNew();
-  } 
+	this.ifOver = function() {
+		var message;
+		if(this.currentPlayer.isWinner()) {
+			message = this.currentPlayer === thePlayer ? "You Win!" : "You Lose!";	
+		} else if (theBoard.isAllFilledIn()) {
+			message = "Tie Game!";
+		} else {
+			return false;
+		}
+		setTimeout(function() {
+			interface.renderGame(message);
+		}, 1000);
+		gameEnded = true;
+	}
+	  
+	this.resetGame = function() {
+		theBoard.resetSquares();
+		thePlayer.resetScore();
+		theComputer.resetScore();
+		gameEnded = false;
+		moveNumber = 1;
+		interface.renderNew();
+	} 
 }
 
 
